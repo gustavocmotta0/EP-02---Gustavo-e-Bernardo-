c3b1eb631753a4af6c8a5e6705f85f2cd3782f4d
Bernardo Horta <bernardoelh@al.insper.edu.br>
2026-04-08 18:16:42 -0300

    FUNC 1

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,7 @@
-import random as rd
-def rolar_dados(n)
-    lista[]
+import random
+def rolar_dados(n):
+    lista = []
+    for i in range(0,n):
+        lista.append(random.randint)
+    return lista
+print (rolar_dados(3))
