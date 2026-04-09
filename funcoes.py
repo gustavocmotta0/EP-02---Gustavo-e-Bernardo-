1921db1d45b0d311096cc607d25b8a595ce653c9
Bernardo Horta <bernardoelh@al.insper.edu.br>
2026-04-09 14:12:37 -0300

    correção função ex -1

--- funcoes.py
+++ funcoes.py
@@ -2,6 +2,7 @@ import random
 def rolar_dados(n):
     lista = []
     for i in range(0,n):
-        a = random.randint(0,6)
+        a = random.randint(1,6)
         lista.append(a)
     return lista
+print(rolar_dados(5))
