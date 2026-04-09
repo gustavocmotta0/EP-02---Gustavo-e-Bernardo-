7343dee88427002b868800305e8a593ce644f5e5
Bernardo Horta <bernardoelh@al.insper.edu.br>
2026-04-09 14:22:54 -0300

    EX 2

--- funcoes.py
+++ funcoes.py
@@ -5,4 +5,7 @@ def rolar_dados(n):
         a = random.randint(1,6)
         lista.append(a)
     return lista
-
+def guardar_dado(lista_dados, lista_dg, i):
+    lista_dg.append(lista_dados[i])
+    fim = [lista_dados,lista_dg] 
+    return fim
