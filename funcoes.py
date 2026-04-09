2eff19940e65d28d5e1a9d40a7cc51d8dea98fdb
Gustavo C. da Motta <gustavocm11@al.insper.edu.br>
2026-04-09 14:56:14 -0300

    EX 3

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+
 import random
 def rolar_dados(n):
     lista = []
@@ -5,7 +6,14 @@ def rolar_dados(n):
         a = random.randint(1,6)
         lista.append(a)
     return lista
+
 def guardar_dado(lista_dados, lista_dg, i):
     lista_dg.append(lista_dados[i])  
     lista_dados.pop(i)              
-    return [lista_dados, lista_dg]  
+    return [lista_dados, lista_dg]
+
+
+def remover_dado(rolados, guardados, indice_removido):  
+    x = [rolados, guardados]
+    return x
+
