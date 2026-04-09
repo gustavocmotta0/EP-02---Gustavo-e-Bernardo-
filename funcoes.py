e1235bf92d62ca348284585687e5f96bf53dbcec
Gustavo C. da Motta <gustavocm11@al.insper.edu.br>
2026-04-09 15:09:13 -0300

    EX3 versao correta

--- funcoes.py
+++ funcoes.py
@@ -19,9 +19,5 @@ def remover_dado(rolados, guardados, indice_removido):
     x = [rolados, guardados]
     return x
 
-dados_rolados = [2, 2, 2, 2]
-dados_no_estoque = [1]
-dado_para_remover = 0
 
-print(remover_dado(dados_rolados, dados_no_estoque, dado_para_remover))
 
