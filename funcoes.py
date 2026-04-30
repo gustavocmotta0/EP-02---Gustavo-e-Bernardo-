6fa3395062109662840c2403d8b61493dde936dc
Gustavo C. da Motta <gustavocm11@al.insper.edu.br>
2026-04-30 14:54:51 -0300

    ex 11.4

--- funcoes.py
+++ funcoes.py
@@ -99,7 +99,7 @@ def calcula_pontos_regra_avancada(dados):
         'cinco_iguais': calcula_pontos_quina(dados),
         'full_house': calcula_pontos_full_house(dados),
         'quadra': calcula_pontos_quadra(dados),
-        'sem_combinacao': calcula_pontos_regra_simples(dados),
+        'sem_combinacao': calcula_pontos_soma(dados),
         'sequencia_alta': calcula_pontos_sequencia_alta(dados),
         'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)}
 
