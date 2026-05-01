55a617e570bed37fa9e6f770f51a6c8961d3aca3
Bernardo Horta <bernardoelh@al.insper.edu.br>
2026-05-01 14:37:44 -0300

    Imprime cartela

--- programa.py
+++ programa.py
@@ -7,30 +7,24 @@ from funcoes import rolar_dados
 from funcoes import guardar_dado
 from funcoes import remover_dado
 
-imprime_cartela(faz_jogada(rolar_dados(0),None,{}))
-jogador_1 = 
-indicej1 = 1
-jogador_2 = 
-indicej2 = 2
-imprime_cartela()
-dados_rolados_1 = rolar_dados(n)
-dados_rolados_2 = rolar_dados(n)
-dg = [] #dados guardados
-print(dados_rolados_1)
-k1 = 0
-for i in range(12):
-    print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
-    acao = input()
-    if(acao == 1):
-        print("Digite o índice do dado a ser guardado (0 a 4):")
-        l = input()
-        dg = guardar_dado(n)[1]
-        print()
-    if(acao == 2):
-        print("Digite o índice do dado a ser removido (0 a 4):")
-    if(acao == 3 and k1 <=1):
-            rolar_dados
-        
+cartela = {
+    'regra_simples': {1: -1, 2: -1, 3: -1, 4: -1, 5: -1, 6: -1},
+    'regra_avancada': {
+        'sem_combinacao': -1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
+ 
+combinacoes_simples = ["1", "2", "3", "4", "5", "6"]
+combinacoes_avancadas = ["sem_combinacao", "quadra", "full_house", "sequencia_baixa", "sequencia_alta", "cinco_iguais"]
+todas_combinacoes = combinacoes_simples + combinacoes_avancadas
+ 
+imprime_cartela(cartela)
+
 
 
 
